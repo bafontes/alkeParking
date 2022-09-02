1da9064df7a779cb4e9efc4f520cb9e9bd792535
Bruno Andres Fontes <bfontes@internal-admins-internal-traffic-l-6ad629-1124484131.us-east-1.elb.amazonaws.com>
2022-09-02 10:37:52 -0300

    first commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+func myFunction(_ completion: @escaping () -> Void) {
+//    DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+    jkjgjg
+    gjgjgj
+        completion()
+//    }
+}
+
+var some = 5
+
+myFunction {
+    some += 1
+    print(some)
+}
+
+some = 7
+print(some)
